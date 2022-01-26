276653474c505f69da167a978296977a79e3406b
Anderson Dantas <andersondantas81@gmail.com>
2022-01-26 18:16:32 -0300

    first commit

---
+++ src/main/java/com/andersondantas/apitorcida/ApiTorcidaApplication.java
@@ -0,0 +1,13 @@
+package com.andersondantas.apitorcida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTorcidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTorcidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersondantas/apitorcida/apitorcida/ApiTorcidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersondantas.apitorcida.apitorcida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTorcidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
